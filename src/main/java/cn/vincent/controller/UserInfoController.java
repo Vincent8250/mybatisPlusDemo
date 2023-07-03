7d3ff70284abc5d40cc0858393d91195ec37846d
c_mengziming <825024711@qq.com>
2023-07-03 17:42:00 +0800

    测试

--- src/main/java/cn/vincent/controller/UserInfoController.java
+++ src/main/java/cn/vincent/controller/UserInfoController.java
@@ -1,7 +1,15 @@
 package cn.vincent.controller;
 
+import cn.vincent.entity.UserInfo;
+import cn.vincent.mapper.UserInfoMapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,5 +22,15 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/userInfo")
 public class UserInfoController {
+
+    @Autowired
+    UserInfoMapper userInfoMapper;
+
+    @ResponseBody
+    @GetMapping("/getUserInfo")
+    public String getUserInfo(){
+        List<UserInfo> userInfos = userInfoMapper.selectList(new QueryWrapper<>());
+        return userInfos.toString();
+    }
 
 }
